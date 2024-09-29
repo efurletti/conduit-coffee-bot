60d303523240cf27ef93a6aa54b6982cfdefdc63
ericfj <eric.furletti@gmail.com>
2024-09-29 13:47:26 -0700

    using nsec to generate Nostr event

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-console.log(
-    "hello world"
-)
+import { generateSecretKey, getPublicKey, getEventHash, finalizeEvent } from "nostr-tools";
+import { bytesToHex, hexToBytes } from '@noble/hashes/utils'
+import fs from "fs";
+import 'dotenv/config'
+
+export function createNostrEvent() {
+    const privateKey = process.env.BOT_NSEC;
+
+    const publicKey = getPublicKey(privateKey);
+
+    const eventData = {
+        kind: 1,
+        created_at: Math.floor(Date.now() / 1000),
+        tags: [],
+        content: 'Hello, Nostr!',
+        pubkey: publicKey
+    };
+
+    const id = getEventHash(eventData);
+
+    const event = {
+        ...eventData,
+        id: id,
+        sig: finalizeEvent({ ...eventData, id }, privateKey)
+    };
+
+    console.log('Private Key:', privateKey.toString('hex'));
+    console.log('Public Key:', publicKey);
+    console.log('Signed Event:', event);
+
+    fs.writeFileSync(`./export/${event.id}.json`, JSON.stringify(event, null, 2));
+};
+
+createNostrEvent();
